3296792bdfce6a9a02cf8e73fd5bc7d67c656f2f
NSIX06 <felipebugalho2016@gmail.com>
2025-03-20 21:21:14 -0400

    criação do App "Categoria"

---
+++ sys_estoque/categoria/serializers.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework import serializers
+from .models import Categoria
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
